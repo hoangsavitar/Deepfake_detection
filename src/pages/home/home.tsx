c45c7fef5e03a5513349e53e90c8013bb4a1f000
phanhuyhiep <hiep2003dev@gmail.com>
2024-12-10 09:00:19 +0700

    ui

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,22 @@
-import { Image } from "antd";
+import { Button, Image } from "antd";
 import "./home.css";
+import { UploadOutlined } from "@ant-design/icons";
 
 const HomePage = () => {
   return (
     <div className="container">
-
       <div>
-        <Image width="550px" src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
+        <Image
+          width="400px"
+          src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+        />
       </div>
+      <Button icon={<UploadOutlined />}>Click to Upload</Button>
       <div>
-
+        <Image
+          width="400px"
+          src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+        />
       </div>
     </div>
   );
